61409f10e483ff6c8b3168185090a36f9cf3f043
Aviral Singh <aviralsingh3501@gmail.com>
2024-05-15 15:15:26 +0530

    settting up logo component

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from './Logo';
 
 const Header = () => {
   return (
-    <div>
+    <div className=''>
         <nav>
             {/* {logo} */}
             <Logo/>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import Link from "next/link";
 import Image from 'next/image'; // Ensure this import is present
+import { AspectRatio } from './ui/aspect-ratio';
 
 function Logo() {
   return (
-    <Link href="/">
-        <Image src="/images/logos/logo.svg" alt="Logo" width={50} height={50} />
+    <Link href="/" prefetch={false} className=' overflow-hidden'>
+        
+        <div className="flex items-center w-72 h-14">
+            <AspectRatio
+            ratio={16/9}
+            className='flex items-center justify-center'
+            >
+                <Image
+                priority
+                src="/images/logos/logo.svg"
+                alt="Polyglot Logo"
+                width={50}
+                height={50}
+                className='dark:filter dark:invert'
+                // invert the color of the logo on swtiching modes : theme
+                 />
+
+            </AspectRatio>
+
+        </div>
     </Link>
   );
 }
