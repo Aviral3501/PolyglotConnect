86f14171d2b3935efe398059279e03a6c6faf266
Aviral Singh <aviralsingh3501@gmail.com>
2024-07-15 10:49:18 +0530

    Firebase coneection resolved

--- auth.ts
+++ auth.ts
@@ -2,25 +2,16 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { adminDB } from "./firebase-admin";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
-// connecting with firebase adapter 
 
-// ----------------------------------------------------------------
-
-
-// ----------------------------------------------------------------
-
-
-export const authOptions:NextAuthOptions={
+export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID!,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET!,
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
-
     ],
-    session:{
-        strategy:'jwt',
+    session: {
+        strategy: 'jwt',
     },
-    adapter:FirestoreAdapter(adminDB),
-
-}satisfies NextAuthOptions  //adding type safety
+    adapter: FirestoreAdapter(adminDB) as any, // Example of using 'as any' for temporary type assertion
+};
